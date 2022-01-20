28941d1ba45e3d0a6001ab0b8fc3bcd66f161abc
Tosi12354 <88394539+Tosi12354@users.noreply.github.com>
2022-01-20 16:05:51 +0800

    Update backstage-template.blade.php

--- resources/views/layouts/backstage-template.blade.php
+++ resources/views/layouts/backstage-template.blade.php
@@ -41,7 +41,7 @@
               <a class="nav-link" href="/blog">最新消息</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="index-about.html">關於我們</a>
+              <a class="nav-link" href="#">關於我們</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">觀星地點</a>
